d15208299bff09d3420b6883f04606cde1f69aab
Cornelius Schaub <github@cornelius-schaub.com>
2020-12-02 03:10:25 +0100

    adding await

--- L07/server/server.ts
+++ L07/server/server.ts
@@ -1,54 +1,59 @@
-import * as http from "http";
+import * as Http from "http";
 import * as Url from "url";
+import * as Mongo from "mongodb";
 
-let server = http.createServer();
 
-let port: number | string | undefined = process.env.PORT;
-if (port == undefined)
-    port = 5001;
+const uri = process.env.MONGODB_URL;
+const client = new Mongo.MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+let recipes: Mongo.Collection;
 
-console.log("server starting on port:" + port);
+startHTTPServer();
+startDBConnection();
 
-server.listen(port);
-server.addListener("request", handleRequest);
+function startHTTPServer() {
+    let server: Http.Server = Http.createServer();
 
-function handleRequest(_request: http.IncomingMessage, _response: http.ServerResponse): void {
+    let port: number | string | undefined = process.env.PORT;
+    if (port == undefined)
+        port = 5001;
+
+    console.log("Server starting on port:" + port);
+
+    server.listen(port);
+    server.addListener("request", handleRequest);
+}
+
+function startDBConnection() {
+    client.connect(() => {
+        console.log("connected to database");
+        recipes = client.db("eia2").collection("hexenkessel");
+    });
+}
+
+async function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): Promise<void> {
     console.log("What's up?");
 
-    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.setHeader("content-type", "application/json; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
 
     if (_request.url) {
-        console.log(_request);
+
         let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
 
-        let jsonObj: any = {};
-        for (let key in url.query) {
+        if (url.query["type"] === "post") {
+            let recipe: any = JSON.parse(<string>url.query["recipe"]);
+            await recipes.insertOne(recipe);
 
-            if (isJson(url.query[key])) {
-                // @ts-ignore
-                jsonObj[key] = JSON.parse(url.query[key])
-            } else {
-                jsonObj[key] = url.query[key]
-            }
+            let jsonString: string = JSON.stringify(recipe);
+            _response.write(jsonString);
+        } else if (url.query["type"] === "get") {
+            const allRecipes = await recipes.find().toArray();
+            _response.write(JSON.stringify(allRecipes));
         }
-
-        let jsonString: string = JSON.stringify(jsonObj);
-        _response.write(jsonString);
     }
 
     _response.end();
 }
-
-function isJson(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
-
 
 
 //Hier Sophie's Code:
@@ -63,3 +68,4 @@ async function submitButtonHandler(_event:Event): Promise <void> {
     alert(JSON.stringify(data));
     console.log(query.toString());
 }*/
+
